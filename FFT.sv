16794a6067ce490ded9cb01bf7c789c33ab1e27f
Kenny Chan <chankenny900@gmail.com>
2020-02-09 02:47:42 -0800

    Adding mic_translator, setting up FFT top level module

--- FFT.sv
+++ FFT.sv
@@ -1,13 +1,32 @@
-module FFT(input clk,		  
+module FFT(input clk, input DOUT, input LRCLK, output BCLK, 		  
 			  output vsync, output hsync, output [3:0] r, output [3:0] g, output [3:0] b);
 	
+	wire new_t, done;
 	wire [15:0] t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15;	//16 bit amplitudes
-	reg [31:0] f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15;
+	wire [15:0] f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15;
+	
+	
+	mic_translator main_translator(	.DOUT		(DOUT),
+												.LRCLK 	(LRCLK),
+												.BCLK		(BCLK),
+												.new_t	(new_t),
+												.t0		(t0),		.t1		(t1),		.t2		(t2),		.t3		(t3),
+												.t4		(t4),		.t5		(t5),		.t6		(t6),		.t7		(t7),
+												.t8		(t8),		.t9		(t9),		.t10		(t10),	.t11		(t11),
+												.t12		(t12),	.t13		(t13),	.t14		(t14),	.t15		(t15)	);
+	
+	FFT_Processor main_professor(		.clk		(clk),
+												.new_t	(new_t),
+												.t0		(t0),		.t1		(t1),		.t2		(t2),		.t3		(t3),
+												.t4		(t4),		.t5		(t5),		.t6		(t6),		.t7		(t7),
+												.t8		(t8),		.t9		(t9),		.t10		(t10),	.t11		(t11),
+												.t12		(t12),	.t13		(t13),	.t14		(t14),	.t15		(t15),
+												.f0		(f0),		.f1		(f1),		.f2		(f2),		.f3		(f3),
+												.f4		(f4),		.f5		(f5),		.f6		(f6),		.f7		(f7),
+												.f8		(f8),		.f9		(f9),		.f10		(f10),	.f11		(f11),
+												.f12		(f12),	.f13		(f13),	.f14		(f14),	.f15		(f15),	
+												.done		(done)	);
 	
 	//video_sync_generator vga(clk, vsync, hsync, r, g, b);
-	
-	FFT_Processor main_professor(t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15,
-										  f0, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15);
-	
 	
 endmodule
---
+++ mic_translator.sv
@@ -0,0 +1,24 @@
+module mic_translator(input DOUT, input LRCLK, output BCLK, output new_t,
+							 output [9:0] t0, output [9:0] t1, output [9:0] t2, output [9:0] t3, 
+							 output [9:0] t4, output [9:0] t5, output [9:0] t6, output [9:0] t7, 
+							 output [9:0] t8, output [9:0] t9, output [9:0] t10, output [9:0] t11, 
+							 output [9:0] t12, output [9:0] t13, output [9:0] t14, output [9:0] t15);
+
+	assign t0 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t1 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t2 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t3 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t4 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t5 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t6 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t7 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t8 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t9 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t10 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t11 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t12 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t13 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t14 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+	assign t15 = {DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT, DOUT};
+
+endmodule
